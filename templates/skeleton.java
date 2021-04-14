f6f2a8ac913499f972a9fe688cffe5ea4309c96c
BB33 <57079653+BB-33@users.noreply.github.com>
2021-04-14 11:02:21 +0800

    Add files via upload
    Add these to /.vim/templates so the /.vimrc can find them

---
+++ templates/skeleton.java
@@ -0,0 +1,17 @@
+
+/**
+* @author Jono Jones
+**/
+
+import CITS2200.*
+
+public class className {
+
+	public static void main(String[] args) {
+		
+		
+		...
+
+
+	}
+}
---
+++ templates/skeleton.py
@@ -0,0 +1,14 @@
+#imports
+
+#functions go here
+
+
+#main
+
+def main():
+    ... 
+
+
+#execution mode
+    if __name__ == "__main__":
+            main()
